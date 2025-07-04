dbf3e0dc0204c07176ed199ded0cda8929c19800
vignesh0764 <crazyvideos467@gmail.com>
2025-07-04 10:34:22 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 5;
+
+console.log("Arithmetic Operators:");
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+
+console.log("\nComparison Operator");
+console.log("Equal:", a == b);
+console.log("Not Equal:", a!=b);
+
+let q=2;
+let r='2';
+console.log("Equal with type correction:", q == r);
+console.log("Strict Equal:",a == b);
+console.log("Stict not Equal:",a!=b);
+console.log("\nBitwise Operators:");
+console.log("Bitwise AND:", a&b);
+console.log("Bitwise OR:", a | b);
+console.log("Bitwise XOR:",a^b);
+console.log("Left Shift:", a<<1);
+console.log("Right Shift:", a>>1);
+let age = 18;
+let canVote = (age >= 18)? "You can vote." : "You cannot vote";
+console.log("\nTernary Operator");
+console.log("Result:", canVote)
